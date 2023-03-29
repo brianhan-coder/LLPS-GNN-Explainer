cc29b53b8cf7e5bf209b171ce0f5906cef806524
brianhan-coder <82759867+brianhan-coder@users.noreply.github.com>
2023-03-28 22:01:26 -0400

    Update data.py

--- data.py
+++ data.py
@@ -50,3 +50,66 @@ for i, data in enumerate(test_dataset):
     print(data)
     if i == 10:
         break
+
+### mini-batching of graphs, adjacency matrices are stacked in a diagonal fashion. 
+### Batching multiple graphs into a single giant graph
+from torch_geometric.loader import DataLoader
+print(len(train_dataset))
+print(batch_size)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+
+print('For each batch in train_loader:')
+for i, data in enumerate(train_loader):
+    print(i)
+    print(data.ptr)
+    print(data)
+    print(data.y)
+#     print(data.batch[:10000])
+    print()
+
+import GNN_core
+
+from importlib import reload
+reload(GNN_core)
+
+from torch_geometric.loader import DataLoader
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+### core GNN 
+num_node_features=len(graph_dataset[0].x[0])
+num_classes=2
+
+if arch == 'GCN':
+    model = GNN_core.GCN(hidden_channels,num_node_features=num_node_features,num_classes=num_classes,num_layers=num_layers)
+if arch == 'GNN':
+    model = GNN_core.GNN(hidden_channels,num_node_features=num_node_features,num_classes=num_classes,num_layers=num_layers)
+if arch == 'GTN':
+    model = GNN_core.GTN(hidden_channels,num_node_features=num_node_features,num_classes=num_classes,num_layers=num_layers)
+
+print(type(model))  
+# for i, layer in enumerate(model.children()):
+#     print(i)
+#     print(layer)
+#     print(isinstance(layer, GCNConv))
+#     dic = layer.state_dict()
+#     print(dic)
+# #     for k in dic:
+# #         print(dic[k])
+
+### randomly initialize GCNConv model parameters
+for layer in model.children():
+    if isinstance(layer, GCNConv):
+        dic = layer.state_dict()
+        for k in dic:
+            dic[k] = torch.randn(dic[k].size())
+        layer.load_state_dict(dic)
+        del(dic)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=200, gamma=0.0001)
+criterion = torch.nn.CrossEntropyLoss()
+# criterion = torch.nn.BCEWithLogitsLoss()
+
+best_val_acc = 0
+best_val_epoch = 0
+best_model=None
